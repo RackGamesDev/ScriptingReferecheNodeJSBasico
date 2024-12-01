59a06567b6bd8e628d360485d988a856eb14dcc3
RackGamesDev <137448965+RackGamesDev@users.noreply.github.com>
2024-12-02 00:31:00 +0100

    Update rutas.js

--- mongodb_mongoose/rutas.js
+++ mongodb_mongoose/rutas.js
@@ -1,25 +1,25 @@
 import {Router} from "express";
 import userModel from "./schemas/user-schema.js";
 
-//obviamente, habria que cubrir los errores manualmente
+//Obviamente, habria que cubrir los errores manualmente
 const ruta = Router();
 ruta.get("/get/:id", async (req,res) => {
-    const user = await userModel.findById(req.params.id).exec();//devuelve un objeto de ese esquema(userModel) que tenga esa id
+    const user = await userModel.findById(req.params.id).exec();//Devuelve un objeto de ese esquema(userModel) que tenga esa id
     if (!user) return res.status(404).send();
     res.send(user.name);
 });
 
 ruta.post("/post", async (req,res) => {
     const {bid, bname} = req.body;
-    const newUser = new userModel({_id: bid, name: bname});//creando una instancia (debe coincidir con el esquema, sobretodo el _id)
-    await newUser.save();//agregandolo a la base de datos
+    const newUser = new userModel({_id: bid, name: bname});//Creando una instancia (debe coincidir con el esquema, sobretodo el _id)
+    await newUser.save();//Agregandolo a la base de datos
     res.send("todo bien");
 });
 
 ruta.delete("/delete/:id", async (req, res) => {
     const user = await userModel.findById(req.params.id).exec();
     if (!user) return res.status(404).send();
-    await user.deleteOne();//borrar una instancia
+    await user.deleteOne();//Borrar una instancia
     res.send("todo bien");
 });
 
