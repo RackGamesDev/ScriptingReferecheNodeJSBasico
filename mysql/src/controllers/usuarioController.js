09430bf033846228f06c132394c36fe43bf2af9f
RackGamesDev <137448965+RackGamesDev@users.noreply.github.com>
2024-12-02 00:32:03 +0100

    Update usuarioController.js

--- mysql/src/controllers/usuarioController.js
+++ mysql/src/controllers/usuarioController.js
@@ -1,10 +1,10 @@
 const controller = {};
 import path from 'path';
 controller.list = (req, res) => {
-    req.getConnection((err, conn) => {//al estar activado el middleware de express-myconnection se pueden usar sus funciones aqui tambien, err es si hay errores
-        conn.query('SELECT * FROM usuarios', (err, usuarios) => {//ejecutar la consulta, como esta devuelve datos usuarios es el array con los datos devueltos
+    req.getConnection((err, conn) => {//Al estar activado el middleware de express-myconnection se pueden usar sus funciones aqui tambien, err es si hay errores
+        conn.query('SELECT * FROM usuarios', (err, usuarios) => {//Ejecutar la consulta, como esta devuelve datos usuarios es el array con los datos devueltos
             if (err) res.json(err);
-            console.log(usuarios);//el array devuelto con todos los usuarios
+            console.log(usuarios);//El array devuelto con todos los usuarios
             res.send(usuarios);
             //res.render('usuarios.html', {data: 123})
         });
@@ -13,7 +13,7 @@ controller.list = (req, res) => {
 controller.save = (req, res) =>{
     const data = req.body;
     req.getConnection((err, conn) => {
-        conn.query('INSERT INTO customer set ? ? ?', [data], (err, usuario));//esta consulta modifica los datos, cada ? es una posicion del array data
+        conn.query('INSERT INTO customer set ? ? ?', [data], (err, usuario));//Esta consulta modifica los datos, cada ? es una posicion del array data
         res.send("ya");
     });
 }
