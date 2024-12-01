12a189c4f8028df1132b2ba5d75c8d01ce1c2229
RackGamesDev <137448965+RackGamesDev@users.noreply.github.com>
2024-12-02 00:33:50 +0100

    Update server.js

--- subir archivo/server.js
+++ subir archivo/server.js
@@ -4,15 +4,15 @@ import * as url from 'url';
 import mimeTypes from 'mime-types';
 //npm i cors para cosas con ajax
 
-import multer from 'multer';//multer funciona como middleware
+import multer from 'multer';//Multer funciona como middleware
 const storage = multer.diskStorage({
-    destination: 'uploads/',//donde se guarda
-    filename: function(req, file, cb){//como sera tratado el nombre del archivo, donde req es la request, cb es el next() y file es el archivo
-        cb("",file.originalname + Date.now() + "." + mimeTypes.extension(file.mimetype));//al terminar se usa la callback (normalmente se le va poniendo un numero a cada archivo, en este caso se guarda con la fecha) (mimetypes devuelve la extension del archivo)
+    destination: 'uploads/',//Donde se guarda
+    filename: function(req, file, cb){//Como sera tratado el nombre del archivo, donde req es la request, cb es el next() y file es el archivo
+        cb("",file.originalname + Date.now() + "." + mimeTypes.extension(file.mimetype));//Al terminar se usa la callback (normalmente se le va poniendo un numero a cada archivo, en este caso se guarda con la fecha) (mimetypes devuelve la extension del archivo)
     }
 });
 const upload = multer({
-    storage: storage//especificar el storage ya configurado
+    storage: storage//Especificar el storage ya configurado
 });
 
 
@@ -23,7 +23,7 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 })
 
-app.post("/files",upload.single('avatar') , (req, res) => {//se usa el middleware multer como single para un solo archivo (especificar el nombre del input file del html)
+app.post("/files",upload.single('avatar') , (req, res) => {//Se usa el middleware multer como single para un solo archivo (especificar el nombre del input file del html)
     res.send("ok");
 })
 
