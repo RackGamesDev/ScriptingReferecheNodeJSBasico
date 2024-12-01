847f73b7c511c591976b4cdc3d277e532708c1e8
RackGamesDev <137448965+RackGamesDev@users.noreply.github.com>
2024-12-02 00:32:47 +0100

    Update app.js

--- mysql/src/app.js
+++ mysql/src/app.js
@@ -1,6 +1,6 @@
 //npm install express mysql express-myconnection morgan ejs
 import express from 'express';
-//import morgan from 'morgan';//morgan pone en consola informacion de las peticiones, solo para dev
+//import morgan from 'morgan';//Morgan pone en consola informacion de las peticiones, solo para dev
 import mysql from 'mysql';
 import myConnection from 'express-myconnection';
 const app = express();
@@ -12,12 +12,12 @@ app.set('view engine', 'ejs');
 //app.set('views', './views');
 
 //app.use(morgan('dev'));
-app.use(myConnection(mysql, {//abrir xampp, abrir conexion con mysql workbench y crear base de datos adentro
-    host: 'localhost',//url de la base de datos, localhost para 127.0.0.1
+app.use(myConnection(mysql, {//Abrir xampp, abrir conexion con mysql workbench y crear base de datos adentro
+    host: 'localhost',//Url de la base de datos, localhost para 127.0.0.1
     user: 'root',
     password: '',
-    port: 3306,//usar este
-    database: 'usuarios'//base de datos concreta/shcema dentro del mysql
+    port: 3306,//Usar este
+    database: 'usuarios'//Base de datos concreta/shcema dentro del mysql
 }, 'single'));
 
 app.use('/', customerRoutes);
